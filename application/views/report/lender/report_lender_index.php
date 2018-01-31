0b46057d8e86584a298eb87c96ca26f1ab4a25c2
auliafaizahr <zahaulia@gmail.com>
2018-01-31 13:07:40 +0700

    tambah folder rekap lender

---
+++ application/views/report/lender/report_lender_index.php
@@ -0,0 +1,253 @@
+<div class="row wrapper border-bottom white-bg page-heading">
+	<div class="col-lg-12">
+		<h2>Laporan Persebaran Pinjaman Luar Negeri Pada Green Book</h2>
+		<ol class="breadcrumb">
+			<li>
+				Halaman ini memberikan informasi dan laporan terkait laporan Pinjaman Luar Negeri berupa statistiknya pada Green Book
+			</li>
+		</ol>
+	</div>
+	<div class="col-lg-2">
+		
+	</div>
+</div>
+
+<div class="wrapper wrapper-content animated fadeIn">
+	<div class="row">
+		<div class="col-lg-3">
+			<div class="ibox float-e-margins">
+				<div class="ibox-title">
+                    <h5>Filter</h5>
+
+                    <div class="ibox-tools">
+                    	<a class="collapse-links">
+                    		<i class="fa fa-chevron-up"></i>
+                    	</a>
+                    </div>
+				</div>
+				<div id="loader1" class="loader1" style='visibility: hidden;'></div>
+				<div class="ibox-content" >
+
+				 
+				 <form role="form" method="POST" action="<?php ?>" enctype="multipart/formdata">
+				 	<div class="form-group">
+				 		<label>Greenbook</label>
+				 		<select class="form-control m-b" name="kode_gb" id="kode_gb">
+				 			<option value=""> Silahkan Pilih </option>
+				 			<?php foreach ($greenbook as $key => $value) { ?>
+
+				 				<option value="<?php echo $value['id']; ?>"> <?php echo $value['nama']; ?></option>';
+				 			<?php } ?>
+				 		</select>
+				 	</div>
+				 	<button id="submit_btn" class="btn btn-primary">Submit</button>
+				 	<button id="tes" class="btn btn-primary">tes</button>
+				 </form>
+				 </div>
+				</div>
+			</div>
+
+			<div class="col-lg-9">
+		<!-- 	untuk tabel dan grafik per kementerian -->
+				<div class="ibox float-e-margins">
+					<div class="ibox-title">
+						<h5>Tabel dan Grafik Laporan Persebaran per Kementerian / Lembaga</h5>
+						<div class="ibox-tools">
+						<a class="collapse-link">
+							<i class="fa fa-chevron-up"></i>
+						</a>
+						</div>
+					</div>
+
+					<div class="ibox-content css-animation-box">
+					<div id="chart_"></div>
+				
+					
+				 		
+				 		<p id="cek"></p>
+						
+					</div>
+				</div>
+
+				<div class="ibox float-e-margins">
+					<div class="ibox-title">
+						<h5>Tabel dan Grafik Laporan Perseberan per Lender</h5>
+						<div class="ibox-tools">
+						<a class="collapse-link">
+							<i class="fa fa-chevron-up"></i>
+						</a>
+						</div>
+					</div>
+
+					<div class="ibox-content css-animation-box">
+					
+					<div id="chart_2"></div>
+
+				 		<p id="cek"></p>
+						
+					</div>
+				</div>
+
+				<div class="ibox float-e-margins">
+					<div class="ibox-title">
+						<h5>Tabel dan Grafik Laporan Persebaran per Program</h5>
+						<div class="ibox-tools">
+						<a class="collapse-link">
+							<i class="fa fa-chevron-up"></i>
+						</a>
+						</div>
+					</div>
+
+					<div class="ibox-content css-animation-box">
+				
+					<div id="chart_3"></div>
+				 		<p id="cek"></p>
+						
+					</div>
+				</div>
+
+				<div class="ibox float-e-margins">
+					<div class="ibox-title">
+						<h5>Tabel dan Grafik Laporan Persebaran per Sektor</h5>
+						<div class="ibox-tools">
+						<a class="collapse-link">
+							<i class="fa fa-chevron-up"></i>
+						</a>
+						</div>
+					</div>
+
+					<div class="ibox-content css-animation-box">
+				
+					<div id="chart_4"></div>
+				 		<p id="cek"></p>
+						
+					</div>
+				</div>
+
+				<div class="ibox float-e-margins">
+					<div class="ibox-title">
+						<h5>Tabel dan Grafik Laporan Persebaran per pembagian Infrastruktur dan Non Infra</h5>
+						<div class="ibox-tools">
+						<a class="collapse-link">
+							<i class="fa fa-chevron-up"></i>
+						</a>
+						</div>
+					</div>
+
+					<div class="ibox-content css-animation-box">
+				
+					<div id="chart_5"></div>
+				 		<p id="cek"></p>
+						
+					</div>
+				</div>
+
+				<div class="ibox float-e-margins">
+					<div class="ibox-title">
+						<h5>Tabel dan Grafik Laporan Persebaran per Status</h5>
+						<div class="ibox-tools">
+						<a class="collapse-link">
+							<i class="fa fa-chevron-up"></i>
+						</a>
+						</div>
+					</div>
+
+					<div class="ibox-content css-animation-box">
+				
+					<div id="chart_6"></div>
+				 		<p id="cek"></p>
+						
+					</div>
+				</div>
+				
+				
+				
+			</div>
+			</div>
+
+
+				</div>
+		
+
+<script src="<?php echo base_url(); ?>assets/inspinia/js/jquery-2.1.1.js"></script>
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/modules/data.js"></script>
+<script src="https://code.highcharts.com/modules/drilldown.js"></script>
+<!-- 
+<script src="<?php echo base_url(); ?>assets/highcharts/highcharts.js"></script>
+<script src="<?php echo base_url(); ?>assets/highcharts/modules/data.js"></script>
+<script src="<?php echo base_url(); ?>assets/highcharts/modules/drilldown.js"></script> --> -->
+
+
+<script>
+
+	
+	function showLoading2(){
+		document.getElementById("loader1").style = "visibility: visible";
+		$('#loader1').fadeOut(1000);
+	}
+
+		
+
+
+	
+	
+		
+		//untuk hapus
+		$('#submit_btn').click(function(){
+			
+			var id = $("#kode_gb").val();
+
+
+			function chart(){
+				$.get("<?php echo base_url(); ?>greenbook/filter_kl_gb/"+id, function(data) {
+					$("#chart_").html(data);
+				});
+			}
+
+			function chart2(){
+				$.get("<?php echo base_url(); ?>greenbook/filter_lender_gb/"+id, function(data) {
+					$("#chart_2").html(data);
+				});
+			}
+
+
+			function chart3(){
+				$.get("<?php echo base_url(); ?>greenbook/filter_program_gb/"+id, function(data) {
+					$("#chart_3").html(data);
+				});
+			}
+
+			function chart4(){
+				$.get("<?php echo base_url(); ?>greenbook/filter_sektor_bluebook/"+id, function(data) {
+					$("#chart_4").html(data);
+				});
+			}
+
+			function chart5(){
+				$.get("<?php echo base_url(); ?>greenbook/filter_infra_gb/"+id, function(data) {
+					$("#chart_5").html(data);
+				});
+			}
+
+			function chart6(){
+				$.get("<?php echo base_url(); ?>greenbook/filter_statusumum_gb/"+id, function(data) {
+					$("#chart_6").html(data);
+				});
+			}
+
+			chart();
+			chart2();
+			chart3();
+			chart4();
+			chart5();
+			chart6();
+			console.log(id);
+			console.log("diatas ini kode bb");
+			
+
+			return false;
+		});
+
+	
+</script> 
---
+++ application/views/report/lender/report_lender_program.php
@@ -0,0 +1,208 @@
+					<div id="container_3" style="min-width: 400px; max-width: 1200px; height: 400px; margin: 0 auto"></div>
+					
+					<container>
+  <table class="table table-striped table-hover js-table" id="example0">
+    <thead>
+      <tr>
+        <th>Lender</th>
+        <th>Total Nilai Pinjaman</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($detail as $key => $value): ?>
+   
+    <tr data-toggle="collapse" data-target="#collapse4039" class="clickable">
+      <td><?php echo $value['program']; ?></td>
+      <td><?php echo $value['total']; ?></td>
+   
+     
+      <td>
+        <div class="btn-group btn-group-sm" role="group" aria-label="...">
+          <div class="btn-group " role="group" aria-label="Voir le detail">
+            <a  class="parents js-view-parents" data-href="formation_json_parents" data-id=4039 data-toggle="tooltip" data-placement="top" alt="Voir les details" title="Details">
+              <span class="glyphicon glyphicon-eye-close" aria-hidden="true" style="color:black; margin: 5px;"></span>
+            </a>
+          </div>
+
+        </div>
+        <input type="hidden" name="untuk_id" value="<?php echo $value['id_program'];?>">
+      </td>
+    </tr>
+    <?php endforeach;   ?>
+     <input type="hidden" name="fetch_id" id="fetch_id" value="<?php echo $id; ?>">
+
+    
+</tbody>
+  </table>
+</container>
+
+<script type="text/javascript">
+var id = $("#fetch_id").val();
+
+
+$(document).ready(function(){
+		$('#example0').DataTable({
+			responsive: true,
+			"dom": 'T<"clear">lfrtip',
+			"order": [[ 0, "desc" ]]
+		});
+	});
+$(document).ready(function() {
+
+
+var $table = $('.js-table');
+
+$table.find('.js-view-parents').on('click', function(e) {
+  e.preventDefault();
+  var $btn = $(e.target), $row = $btn.closest('tr'), $nextRow = $row.next('tr.expand-child');
+  $btn.toggleClass('glyphicon-eye-close glyphicon-eye-open');
+  // if .expand-chid row exist already, toggle
+  if ($nextRow.length) {
+      $nextRow.toggle($btn.hasClass('glyphicon-eye-open'));
+  // if not, create .expand-child row
+  } else {
+ // var parentsData = [];
+
+    $.ajax({
+        url: "<?php echo base_url(); ?>usulan/filter_infra_bluebook",
+        dataType: "json",
+        //data: parentsData,
+        success: function (isi) {
+        	console.log(isi);
+        	console.log("inidibawah parentsdata");
+   
+		  var parentsData = {
+		  "success": true,
+		  "parents": isi
+			};
+
+        	data: parentsData;
+
+    var newRow = '<tr class="expand-child" id="collapse' + $btn.data('name') + '">' +
+      '<td colspan="12">' +
+      '<table class="table table-condensed table-bordered" width=100% >' +
+      '<thead>' +
+      '<tr>' +
+      '<th>Surname</th>' +
+      '<th>FirstName</th>' +
+      '<th>School Id</th>' +
+      '<th>School name</th>' +
+      '</tr>' +
+      '</thead>' +
+      '<tbody>';
+
+    if (parentsData.parents) {
+      $.each(parentsData.parents, function(i, parent) {
+        newRow += '<tr>' +
+          '<td>' + parent.name + '</td>' +
+          '<td>' + parent.y + '</td>' +
+        
+          '</tr>';
+      });
+    }
+    newRow += '</tbody></table></td></tr>';
+    // set next row
+    $nextRow = $(newRow).insertAfter($row);
+    
+    
+    }
+        });
+  }
+});
+
+});
+
+	
+	$.ajax({
+				url: "<?php echo base_url(); ?>greenbook/filter_program_isi_gb/"+id,
+				type: "GET",
+				dataType: "html",
+				
+				//dataType: 'json'
+				beforeSend: function(){
+					//notif();
+					//showLoading();
+				},
+				success:function(data,){
+
+					var isi = data;
+					var a = isi.length;
+					console.log(data[0]);
+					console.log(a);
+
+					  
+					$.getJSON("<?php echo base_url(); ?>greenbook/filter_program_isi_gb/"+id, function(json) {
+					console.log(json);
+					console.log(json.length);
+					console.log(json[0].name);
+					//console.log(json1[0].name2);
+
+					var banyak = json.length;
+					var a = json[0].name;
+					var b = parseFloat(json[0].y);
+					//var c = JSON.parse(json);
+					var d = json;
+					//var e = JSON.parse(d);
+					//console.log(e);
+					console.log(b);
+
+					var isi= [];
+					//var isi2= [];
+
+
+					for (var i = banyak - 1; i >= 0; i--) {
+						isi.push({
+						name: [json[i].name],
+						y: parseFloat([json[i].y])
+						});
+					}
+
+					
+					
+
+					Highcharts.chart('container_3', {
+					    chart: {
+					        plotBackgroundColor: null,
+					        plotBorderWidth: null,
+					        plotShadow: false,
+					        type: 'pie'
+					    },
+					    title: {
+					        text: 'Persebaran Bluebook berdasarkan Program'
+					    },
+					    tooltip: {
+					        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+					    },
+					    plotOptions: {
+					        pie: {
+					            allowPointSelect: true,
+					            cursor: 'pointer',
+					            dataLabels: {
+					                enabled: true,
+					                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+					                style: {
+					                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+					                }
+					            }
+					        }
+					    },
+					    series: [{
+
+					    	name: 'Brands',
+        					colorByPoint: true,
+					        data: isi
+					        		
+					    }]
+					});
+
+
+					});
+					
+
+
+					
+				},
+				dataType:"html"});
+
+</script>
