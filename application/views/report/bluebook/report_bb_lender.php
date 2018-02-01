92a6540bc319d8ef6ece684e1f81320bbe87d94d
auliafaizahr <zahaulia@gmail.com>
2018-02-01 11:23:51 +0700

    coba di sebelah

--- application/views/report/bluebook/report_bb_lender.php
+++ application/views/report/bluebook/report_bb_lender.php
@@ -1,4 +1,5 @@
 					<div id="container_8" style="min-width: 400px; max-width: 800px; height: 400px; margin: 0 auto"></div>
+					<div id="container_15" style="min-width: 400px; max-width: 800px; height: 400px; margin: 0 auto"></div>
 					
 					<container>
   <table class="table table-striped table-hover js-table" id="example2">
@@ -150,6 +151,8 @@ $table.find('.js-view-parents').on('click', function(e) {
 
 					var isi= [];
 					var isi2= [];
+					var series = {};
+
 					//var isi2= [];
 
 
@@ -164,30 +167,32 @@ $table.find('.js-view-parents').on('click', function(e) {
 						console.log(isi);
 						console.log("diatas ini isi yg tumpah");
 
+						//ngambil isi untuk drilldown 
+
+
+
 						$.getJSON("<?php echo base_url(); ?>usulan/filter_lender_isi_bluebook_program/"+id+"/"+json[i].id, function(json1) {
 
 							var panjang = json1.length;
-							for (var j = panjang - 1; j >= 0; j--) {
+							/*for (var j = panjang - 1; j >= 0; j--) {
 								
-								isi2.push({
+								isi[i].y.push({
 									name: [json1[j].name],
 									y: parseFloat(json1[j].y)
 								})
 
-							}
-
+							}*/
 							
-
-
-							
+							console.log(panjang);
+							console.log("diatas ini panjang");
 						
 						});
 					
-					console.log(isi[0]);
-							console.log("diatas ini panjang");
+					
 					
 
 					}
+
 
 
 
@@ -234,5 +239,127 @@ $table.find('.js-view-parents').on('click', function(e) {
 					
 				},
 				dataType:"html"});
+
+	// Create the chart
+Highcharts.chart('container_15', {
+    chart: {
+        type: 'pie'
+    },
+    title: {
+        text: 'Browser market shares. January, 2015 to May, 2015'
+    },
+    subtitle: {
+        text: 'Click the slices to view versions. Source: netmarketshare.com.'
+    },
+    plotOptions: {
+        series: {
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}: {point.y:.1f}%'
+            }
+        }
+    },
+
+    tooltip: {
+        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+    },
+    series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'IE',
+            y: 56.33,
+            drilldown: 'IE'
+        }, {
+            name: 'Chrome',
+            y: 24.03,
+            drilldown: 'Chrome'
+        }, {
+            name: 'Firefox',
+            y: 10.38,
+            drilldown: 'Firefox'
+        }, {
+            name: 'Safari',
+            y: 4.77,
+            drilldown: 'Safari'
+        }, {
+            name: 'Opera',
+            y: 0.91,
+            drilldown: 'Opera'
+        }, {
+            name: 'Proprietary or Undetectable',
+            y: 0.2,
+            drilldown: null
+        }]
+    }],
+    drilldown: {
+        series: [{
+            name: 'IE',
+            id: 'IE',
+            data: [
+                ['v11.0', 24.13],
+                ['v8.0', 17.2],
+                ['v9.0', 8.11],
+                ['v10.0', 5.33],
+                ['v6.0', 1.06],
+                ['v7.0', 0.5]
+            ]
+        }, {
+            name: 'Chrome',
+            id: 'Chrome',
+            data: [
+                ['v40.0', 5],
+                ['v41.0', 4.32],
+                ['v42.0', 3.68],
+                ['v39.0', 2.96],
+                ['v36.0', 2.53],
+                ['v43.0', 1.45],
+                ['v31.0', 1.24],
+                ['v35.0', 0.85],
+                ['v38.0', 0.6],
+                ['v32.0', 0.55],
+                ['v37.0', 0.38],
+                ['v33.0', 0.19],
+                ['v34.0', 0.14],
+                ['v30.0', 0.14]
+            ]
+        }, {
+            name: 'Firefox',
+            id: 'Firefox',
+            data: [
+                ['v35', 2.76],
+                ['v36', 2.32],
+                ['v37', 2.31],
+                ['v34', 1.27],
+                ['v38', 1.02],
+                ['v31', 0.33],
+                ['v33', 0.22],
+                ['v32', 0.15]
+            ]
+        }, {
+            name: 'Safari',
+            id: 'Safari',
+            data: [
+                ['v8.0', 2.56],
+                ['v7.1', 0.77],
+                ['v5.1', 0.42],
+                ['v5.0', 0.3],
+                ['v6.1', 0.29],
+                ['v7.0', 0.26],
+                ['v6.2', 0.17]
+            ]
+        }, {
+            name: 'Opera',
+            id: 'Opera',
+            data: [
+                ['v12.x', 0.34],
+                ['v28', 0.24],
+                ['v27', 0.17],
+                ['v29', 0.16]
+            ]
+        }]
+    }
+});
 
 </script>
